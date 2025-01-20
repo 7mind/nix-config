cf697cba8cc9d3fec2bfd5443a21e45e96fb8edd
Pavel Shirshov <pshirshov@eml.cc>
2025-01-20 19:37:15 +0000

    autosave

--- modules/nix/bootloader-lanzaboote.nix
+++ modules/nix/bootloader-lanzaboote.nix
@@ -12,14 +12,15 @@
   config = lib.mkIf config.smind.lanzaboote.enable {
     boot.loader = {
       grub.enable = lib.mkForce false;
+
       efi.efiSysMountPoint = "/boot";
+
       timeout = 2;
+
       systemd-boot = {
         enable = lib.mkForce false;
-        memtest86.enable = false;
-        edk2-uefi-shell.enable = false;
-        consoleMode = "keep";
-        configurationLimit = 3;
+        memtest86.enable = lib.mkForce false;
+        edk2-uefi-shell.enable = lib.mkForce false;
       };
     };
 
@@ -29,6 +30,7 @@
       settings = {
         consoleMode = "keep";
         configurationLimit = 3;
+        reboot-for-bitlocker = true;
       };
     };
   };
