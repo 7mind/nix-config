3299f0302f5e5bad0bdff2357afa161b4484da19
Pavel Shirshov <pshirshov@eml.cc>
2025-03-05 19:46:04 +0000

    autosave

--- globals.nix
+++ globals.nix
@@ -62,13 +62,23 @@ rec {
       cfg-platform =
         if cfg-meta.isLinux then {
           generator = inputs.nixpkgs.lib.nixosSystem;
+
           flake-modules = smind-nixos-imports ++ [
             inputs.lanzaboote.nixosModules.lanzaboote
             inputs.nix-apple-fonts.nixosModules
           ];
+
+          hm-modules = [
+
+          ];
         } else {
           generator = inputs.nixpkgs.lib.nixosSystem;
+
           flake-modules = smind-darwin-imports ++ [
+
+          ];
+
+          hm-modules = [
 
           ];
         };
@@ -78,7 +88,7 @@ rec {
         nix-apple-fonts = inputs.nix-apple-fonts.packages."${arch}";
       };
 
-      cfg-hm-modules = [
+      cfg-hm-modules = cfg-platform.hm-modules ++ [
         inputs.agenix.homeManagerModules.default
       ];
 
@@ -110,81 +120,6 @@ rec {
           ];
         };
     };
-
-
-  # make-nixos = { self, inputs, arch }: hostname:
-  #   let
-  #     pkgs = inputs.nixpkgs.legacyPackages."${arch}";
-
-  #     paths = {
-  #       root = "${self}";
-  #       pkg = "${self}/pkg";
-  #       private = "${self}/private";
-  #       secrets = "${self}/private/secrets";
-  #       lib = "${self}/lib";
-  #       modules = "${self}/modules";
-  #       modules-hm = "${self}/modules/hm";
-  #       modules-nix = "${self}/modules/nix";
-  #       users = "${self}/users";
-  #     };
-
-
-  #     cfg-packages = const.cfg-packages {
-  #       inherit inputs;
-  #       inherit pkgs;
-  #       inherit arch;
-  #     };
-
-  #     cfg-meta = {
-  #       inherit arch;
-  #       inherit paths;
-  #       inherit inputs;
-  #       isLinux = true;
-  #       isDarwin = false;
-  #       hostname = hostname;
-  #       state-version-nixpkgs = cfg-const.state-version-nixpkgs;
-  #     };
-
-  #     cfg-flakes = {
-  #       pkgs7mind = inputs.smind.legacyPackages."${arch}";
-  #       nix-apple-fonts = inputs.nix-apple-fonts.packages."${arch}";
-  #     };
-
-  #     cfg-hm-modules = [
-  #       inputs.agenix.homeManagerModules.default
-  #     ];
-
-
-  #     specialArgs = pkgs.lib.fix (self: {
-  #       inherit smind-hm;
-  #       inherit cfg-meta;
-  #       inherit cfg-flakes;
-  #       inherit cfg-packages;
-  #       inherit cfg-hm-modules;
-  #       inherit inputs;
-  #       inherit cfg-const;
-  #       specialArgsSelfRef = self;
-  #       import_if_exists = path: if builtins.pathExists path then import path else { }; # for some reason I can't add this into lib
-  #     });
-  #   in
-  #   {
-  #     name = "${hostname}";
-  #     value = inputs.nixpkgs.lib.nixosSystem
-  #       {
-  #         inherit specialArgs;
-  #         system = "${arch}";
-  #         modules = smind-nixos-imports ++ [
-  #           inputs.lanzaboote.nixosModules.lanzaboote
-  #           inputs.nix-apple-fonts.nixosModules
-  #           inputs.home-manager.nixosModules.home-manager
-
-  #           inputs.agenix.nixosModules.default
-  #           inputs.agenix-rekey.nixosModules.default
-
-  #           ./hosts/${hostname}/cfg-${hostname}.nix
-  #         ];
-  #       };
-  #   };
 
   make-nixos-x86_64 = { inputs, self }: (make {
     inherit inputs;
