29aa0062a00efc155c02a1eb9299031ba57a424f
Pavel Shirshov <pshirshov@eml.cc>
2025-05-01 00:26:25 +0100

    autosave (pavel-am5 / Linux)

--- pkg/ip-update/ip-update.nix
+++ pkg/ip-update/ip-update.nix
@@ -1,27 +1,14 @@
 { lib, pkgs, stdenvNoCC }:
 
-# substituteAll {
-#   name = "ip-update";
-#   src = ./ip-update.sh;
-#   aws = pkgs.awscli2;
-
-#   dir = "bin";
-#   isExecutable = true;
-
-#   meta = with lib; {
-#     description = "route 53 ip update";
-#     license = [ licenses.mit ];
-#     maintainers = with maintainers; [ pshirshov ];
-#     platforms = platforms.linux;
-#   };
-# }
-
 stdenvNoCC.mkDerivation {
   name = "ip-update";
-  src = replaceVars ./ip-update.sh { aws = pkgs.awscli2; }
+  src = pkgs.replaceVars ./ip-update.sh { aws = pkgs.awscli2; };
 
-  dir = "bin";
-  isExecutable = true;
+  builder = pkgs.writeText "builder.sh" ''
+    mkdir -p $out/bin
+    cp $src $out/bin/$name
+    chmod +x $out/bin/$name
+  '';
 
   meta = with lib; {
     description = "route 53 ip update";
