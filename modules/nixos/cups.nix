3565fe180dc2ea431f3694ed75c0c6c41345786e
Pavel Shirshov <pshirshov@eml.cc>
2025-05-12 18:55:08 +0100

    autosave

--- modules/nixos/cups.nix
+++ modules/nixos/cups.nix
@@ -36,7 +36,7 @@
         {
           name = "Brother";
           location = "Home";
-          deviceUri = "ipp://printer.local:631/ipp/print";
+          deviceUri = "ipp://printer.home.7mind.io:631/ipp/print";
           # offline printer hack
           model = "BrotherDCP.ppd";
           # Initial setup
