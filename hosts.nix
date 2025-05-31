1d14170e1c047c4aad4fceb33dd3b50f6ec905bd
Pavel Shirshov <pshirshov@eml.cc>
2025-05-31 19:43:32 +0100

    autosave

--- hosts.nix
+++ hosts.nix
@@ -1,11 +1,6 @@
 builders: {
   nixos = [
     (builders.make-nixos-x86_64 "pavel-am5")
-    (builders.make-nixos-x86_64 "vm")
-    (builders.make-nixos-x86_64 "nas")
-
-    (builders.make-nixos-aarch64 "o1")
-    (builders.make-nixos-aarch64 "o2")
   ];
 
   darwin = [
