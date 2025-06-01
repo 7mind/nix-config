f37c02b457d4f671c1cb78c4aaafbe7c8126f5bc
Pavel Shirshov <pshirshov@eml.cc>
2025-06-01 20:06:58 +0100

    autosave (pavel-am5 / Linux)

--- modules/hm/nushell.nix
+++ modules/hm/nushell.nix
@@ -28,9 +28,9 @@
          partial: true    # set to false to prevent partial filling of the prompt
          algorithm: "fuzzy"    # prefix or fuzzy
          external: {
-         # set to false to prevent nushell looking into $env.PATH to find more suggestions
+             # set to false to prevent nushell looking into $env.PATH to find more suggestions
              enable: true
-         # set to lower can improve completion performance at the cost of omitting some options
+             # set to lower can improve completion performance at the cost of omitting some options
              max_results: 100
              completer: $carapace_completer # check 'carapace_completer'
            }
