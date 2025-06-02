12296131b847860e2416203d02662e0113ac0e92
Pavel Shirshov <pshirshov@eml.cc>
2025-06-02 19:38:01 +0100

    wip

--- modules/hm/zsh.nix
+++ modules/hm/zsh.nix
@@ -10,32 +10,18 @@
   };
 
   config = lib.mkIf config.smind.hm.zsh.enable {
-    # home.activation.zsh-cleanup = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    #   echo >&2 "Cleaning up zsh junk..."
-    #   rm -f ${config.home.homeDirectory}/.zshrc.zwc
-    #   rm -rf ${config.home.homeDirectory}/.zcompdump* # get rid of outdated zsh junk
-    # '';
-
     programs.zoxide.enableZshIntegration = true;
 
     programs.wezterm.enableZshIntegration = true;
 
     programs.carapace.enableZshIntegration = true;
 
-    # programs.autojump = {
-    #   enable = true;
-    #   enableBashIntegration = true;
-    #   enableZshIntegration = true;
-    # };
-
-    # see any.nix, any-nixos.nix any-darwin.nix, and HM config in zsh.nix
     # https://home-manager-options.extranix.com/?query=programs.zsh&release=master
     programs.zsh = {
       enable = true;
       #zprof.enable = true;
-      autocd = true;
 
-      #autosuggestion.enable = true;
+      autocd = true;
       syntaxHighlighting.enable = true;
 
       history = {
@@ -65,42 +51,10 @@
         what() {
           ls -la `which $1`
         }
-
-        # _fzf_comprun () {
-        #   local command = $1
-        #   shift
-        #   case "$command" in
-        #       cd)           fzf "$@" --preview 'tree -C {} | head -200';;
-        #       *)            fzf "$@" ;;
-        #   esac
-        # }
       '';
     };
 
 
     programs.atuin.enableZshIntegration = true;
-    
-    # programs.fzf = {
-    #   enable = true;
-    #   enableZshIntegration = true;
-    #   defaultCommand = "fd .$HOME";
-    #   fileWidgetCommand = "$FZF_DEFAULT_COMMAND";
-    #   changeDirWidgetCommand = "fd -t d . $HOME";
-    #   defaultOptions = [
-    #     "--layout=reverse"
-    #     "--border"
-    #     "--info=inline"
-    #     "--height=80%"
-    #     "--multi"
-    #     "--preview-window=:hidden"
-    #     "--preview '([[ -f {} ]] && (bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'"
-    #     "--color='hl:148,hl+:154,pointer:032,marker:010,bg+:237,gutter:008'"
-    #     "--prompt='∼ '"
-    #     "--pointer='▶'"
-    #     "--marker='✓'"
-    #     "--bind '?:toggle-preview'"
-    #   ];
-    #   tmux.enableShellIntegration = true;
-    # };
   };
 }
