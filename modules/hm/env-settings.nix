fe28b5ef7837bfd5ab538a2beb54b1eb46d20613
Pavel Shirshov <pshirshov@eml.cc>
2025-06-03 18:45:33 +0100

    autosave (pavel-am5 / Linux)

--- modules/hm/env-settings.nix
+++ modules/hm/env-settings.nix
@@ -50,6 +50,10 @@
     programs.atuin = {
       enable = true;
       settings = {
+        auto_sync = true;
+        sync_frequency = "5m";
+        sync_address = "https://api.atuin.sh";
+
         enter_accept = false;
         prefers_reduced_motion = true;
       };
