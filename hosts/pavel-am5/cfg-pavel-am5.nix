fd89ebb383fea071c43e1946bae60639abc499e5
Pavel Shirshov <pshirshov@eml.cc>
2025-06-06 11:48:01 +0100

    autosave

--- hosts/pavel-am5/cfg-pavel-am5.nix
+++ hosts/pavel-am5/cfg-pavel-am5.nix
@@ -53,15 +53,6 @@
                       {symbol = "BTC-USD"; name="BTC-USD";}
                     ];
                   }
-                  {
-                    type = "monitor";
-                    cache = "1m";
-                    title = "Services";
-                    sites =[
-                      {title = "Home Assistant"; url = "https://ha.7mind.io"; icon = "/static/icons/favicon-apple-180x180.png";}
-                    ];
-
-                  }
                 ];
               }
               {
@@ -73,6 +64,20 @@
                       {type = "hacker-news";}
                       {type=  "lobsters";}
                     ];
+                  }
+                ];
+              }
+                            {
+                size = "small";
+                widgets = [
+                  {
+                    type = "monitor";
+                    cache = "1m";
+                    title = "Services";
+                    sites =[
+                      {title = "Home Assistant"; url = "https://ha.7mind.io"; icon = "/static/icons/favicon-apple-180x180.png";}
+                    ];
+
                   }
                 ];
               }
