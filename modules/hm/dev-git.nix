6b76ea2fc75e9449e26d6de5c29a1352a7319ff4
Pavel Shirshov <pshirshov@eml.cc>
2025-09-24 18:25:49 +0100

    autosave (pavel-am5 / Linux)

--- modules/hm/dev-git.nix
+++ modules/hm/dev-git.nix
@@ -23,7 +23,7 @@
           "!gi() { curl -L -s https://www.gitignore.io/api/visualstudiocode,jetbrains+all,java,scala,sbt,maven,metals ;}; gi";
       };
       extraConfig = {
-        credential.helper = "libsecret";
+        credential.helper = "git-credential-libsecret";
 
         core = {
           reloadindex = true;
