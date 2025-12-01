6b7938815e23df19b5d3c66b05277d5064287f3e
Pavel Shirshov <pshirshov@eml.cc>
2025-12-01 22:26:30 +0000

    autosave (pavel-am5 / Linux)

--- modules/hm/dev-git.nix
+++ modules/hm/dev-git.nix
@@ -13,7 +13,7 @@
     home.packages = with pkgs; [ git-review tig lazygit ];
 
     programs.mergiraf.enable = true;
-    
+
     programs.git = {
       enable = true;
       package = pkgs.gitFull;
@@ -26,36 +26,34 @@
             "!gi() { curl -L -s https://www.gitignore.io/api/visualstudiocode,jetbrains+all,java,scala,sbt,maven,metals ;}; gi";
         };
 
-        extraConfig = {
-          credential.helper = "${pkgs.gitFull}/bin/git-credential-libsecret";
+        credential = { helper = "${pkgs.gitFull}/bin/git-credential-libsecret"; };
 
-          core = {
-            reloadindex = true;
-            whitespace = "fix,-indent-with-non-tab,trailing-space,cr-at-eol";
-            compression = 9;
-            editor = "nano";
-            untrackedcache = true;
-            fsmonitor = true;
-          };
-          push = {
-            default = "simple";
-            autoSetupRemote = true;
-          };
-          pull = { ff = "only"; };
-
-          branch.autosetuprebase = "always";
-
-          receive.fsckObjects = true;
-          status.submodulesummary = true;
-          pack.packSizeLimit = "2g";
-          fetch.prune = "false";
-          rebase.autoStash = true;
-          help.autocorrect = 3;
-          init.defaultBranch = "main";
-          mergetool.keepBackup = "false";
-
-          sequence.editor = "${pkgs.git-interactive-rebase-tool}/bin/interactive-rebase-tool";
+        core = {
+          reloadindex = true;
+          whitespace = "fix,-indent-with-non-tab,trailing-space,cr-at-eol";
+          compression = 9;
+          editor = "nano";
+          untrackedcache = true;
+          fsmonitor = true;
         };
+        push = {
+          default = "simple";
+          autoSetupRemote = true;
+        };
+        pull = { ff = "only"; };
+
+        branch = { autosetuprebase = "always"; };
+
+        receive = { fsckObjects = true; };
+        status = { submodulesummary = true; };
+        pack = { packSizeLimit = "2g"; };
+        fetch = { prune = "false"; };
+        rebase = { autoStash = true; };
+        help = { autocorrect = 3; };
+        init = { defaultBranch = "main"; };
+        mergetool = { keepBackup = "false"; };
+
+        sequence = { editor = "${pkgs.git-interactive-rebase-tool}/bin/interactive-rebase-tool"; };
       };
     };
   };
