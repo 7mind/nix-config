44d42a9afd454bb7f8e781415113c8563641e051
Pavel Shirshov <pshirshov@eml.cc>
2026-01-03 23:37:06 +0000

    autosave (pavel-fw / Linux)

--- modules/hm/ghostty.nix
+++ modules/hm/ghostty.nix
@@ -20,7 +20,7 @@ in
 
     smind.hm.ghostty.theme = lib.mkOption {
       type = lib.types.str;
-      default = "Dark+";
+      default = "Adwaita Dark";
       description = "Ghostty color theme (use 'ghostty +list-themes' to see available)";
     };
   };
