64d4666455cd3533c4ba8de14426c9fae81c020c
Pavel Shirshov <pshirshov@eml.cc>
2026-01-04 15:01:58 +0000

    autosave (pavel-fw / Linux)

--- hosts/pavel-fw/cfg-pavel-fw.nix
+++ hosts/pavel-fw/cfg-pavel-fw.nix
@@ -118,6 +118,7 @@ in
   # Framework-specific services
   services.power-profiles-daemon.enable = true;
   smind.power-management.auto-profile.enable = true;
+  smind.power-management.auto-profile.onAC = "performance";
 
   # QMK keyboard firmware support (Framework 16 uses QMK)
   # Use https://keyboard.frame.work/ for configuration
