6269ffec93cd96cfe194505bb3edbc0213ab6145
Pavel Shirshov <pshirshov@eml.cc>
2026-01-04 12:39:34 +0000

    autosave (pavel-fw / Linux)

--- hosts/pavel-fw/home-pavel.nix
+++ hosts/pavel-fw/home-pavel.nix
@@ -1,29 +1,5 @@
 { pkgs, config, smind-hm, lib, cfg-meta, import_if_exists_or, ... }:
 
-let
-  fractal-kdocker = pkgs.runCommand "fractal-kdocker"
-    {
-      nativeBuildInputs = [ pkgs.makeWrapper ];
-      meta.mainProgram = "fractal";
-    } ''
-        mkdir -p $out/bin $out/share
-
-        # Create wrapper that launches fractal via kdocker (minimized to tray)
-        # Use icon name (not path) so kdocker looks it up from theme
-        cat > $out/bin/fractal <<EOF
-    #!/usr/bin/env bash
-    exec ${pkgs.kdocker}/bin/kdocker -q -o -l -i org.gnome.Fractal ${pkgs.fractal}/bin/fractal "\$@"
-    EOF
-        chmod +x $out/bin/fractal
-
-        # Copy desktop file with updated Exec
-        mkdir -p $out/share/applications
-        sed "s|Exec=fractal|Exec=$out/bin/fractal|g" ${pkgs.fractal}/share/applications/org.gnome.Fractal.desktop > $out/share/applications/org.gnome.Fractal.desktop
-
-        # Symlink icons
-        ln -s ${pkgs.fractal}/share/icons $out/share/icons
-  '';
-in
 {
   imports = smind-hm.imports ++ [
     "${cfg-meta.paths.users}/pavel/hm/home-pavel-generic.nix"
@@ -31,8 +7,7 @@ in
   ];
 
   home.packages = [
-    pkgs.kdocker
-    fractal-kdocker
+    pkgs.fractal
   ];
 
   smind.hm = {
