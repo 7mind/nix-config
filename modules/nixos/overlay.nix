9c95729063aa78aecf17dac224ccd032e7724a8f
Pavel Shirshov <pshirshov@eml.cc>
2026-01-10 01:22:07 +0000

    autosave (pavel-fw / Linux)

--- modules/nixos/overlay.nix
+++ modules/nixos/overlay.nix
@@ -67,12 +67,18 @@
       #   ];
       # });
 
-      # Fix for suspend loop on GNOME with NVIDIA drivers
-      # MR !462: https://gitlab.gnome.org/GNOME/gnome-settings-daemon/-/merge_requests/462
-      # Issue #903: https://gitlab.gnome.org/GNOME/gnome-settings-daemon/-/issues/903
+      # Fix suspend loop with GNOME+NVIDIA
+      # https://github.com/NixOS/nixpkgs/issues/336723#issuecomment-3724194485
+      # https://gitlab.gnome.org/GNOME/gnome-settings-daemon/-/merge_requests/462
+      # https://gitlab.gnome.org/GNOME/gnome-settings-daemon/-/commit/44e1cc564b02349adab38e691770f13c0e09951b
+      # https://github.com/GNOME/gnome-settings-daemon/commit/44e1cc564b02349adab38e691770f13c0e09951b
       gnome-settings-daemon = super.gnome-settings-daemon.overrideAttrs (old: {
-        patches = (old.patches or []) ++ [
-          ../../patches/gnome-settings-daemon-suspend-loop-fix.patch
+        patches = (old.patches or [ ]) ++ [
+          (pkgs.fetchpatch {
+            url = "https://gitlab.gnome.org/GNOME/gnome-settings-daemon/-/commit/44e1cc564b02349adab38e691770f13c0e09951b.patch";
+            name = "fix-gnome-sleep-loop.patch";
+            hash = "sha256-hElYD91/1/LO9SaUYNZaIlzIKmOSVPVpGy9v4PwsTi4=";
+          })
         ];
       });
 
