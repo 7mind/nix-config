d97bd1753aece8f8ad976c723ec7f652c4652c77
Pavel Shirshov <pshirshov@eml.cc>
2026-01-10 01:56:19 +0000

    autosave (pavel-fw / Linux)

--- modules/nixos/overlay.nix
+++ modules/nixos/overlay.nix
@@ -93,16 +93,25 @@
       # Fix for cursor stutter/lag
       # Issue: https://gitlab.gnome.org/GNOME/mutter/-/issues/4518
       # MR !4795: https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4795
-      # DISABLED: Requires libinput >= 1.31, NixOS 25.11 currently has 1.29
-      # mutter = super.mutter.overrideAttrs (old: {
-      #   patches = (old.patches or [ ]) ++ [
-      #     (pkgs.fetchpatch {
-      #       url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4795.patch";
-      #       name = "mutter-fix-cursor-stutter.patch";
-      #       hash = "sha256-3el2d7c7ryWwRDJZ7s5pjcG5/LfJx6sK8ydWQolvfLQ=";
-      #     })
-      #   ];
-      # });
+      # MR !4833: https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4833
+      #
+      # Includes 49.3 backports:
+      # - screencast: Fix cursor stutter/lag by accumulating damage correctly (Closes #4380)
+      # - screencast: Fix damage region coordinates for PipeWire consumers (Closes #4269)
+      # - screencast: Handle blit failures and fallback to drawing
+      # - wayland: Fix subsurface geometry calculation (Closes #4250)
+      # - x11: Fix sync counter issues by reverting per-view frame counter (Closes #4216)
+      # - xwayland: Fix inconsistent layout due to monitor scale updates
+      # - misc: Fix memory leaks in text-accessible and CICP initialization issues (Closes #4534, #4344)
+      mutter = super.mutter.overrideAttrs (old: {
+        patches = (old.patches or [ ]) ++ [
+          (pkgs.fetchpatch {
+            url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4833.patch";
+            name = "mutter-fix-cursor-stutter.patch";
+            hash = "sha256-NM5vO+oTL213KM7P7dfM7DlyZqDf0hMgzqVXNAbjYh0=";
+          })
+        ];
+      });
 
 
 
