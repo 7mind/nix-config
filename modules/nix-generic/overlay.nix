077d198a593d0e267d4dbd1ed6a56b1c7fe0e610
Pavel Shirshov <pshirshov@eml.cc>
2026-02-24 15:50:23 +0000

    autosave (pavel-am5 / Linux)

--- modules/nix-generic/overlay.nix
+++ modules/nix-generic/overlay.nix
@@ -6,13 +6,13 @@
       # Downgrade wireplumber to 0.5.12 to fix GNOME crash when switching
       # Bluetooth audio to handsfree/HSP/HFP profile.
       # See: https://github.com/NixOS/nixpkgs/issues/475202
-      wireplumber = prev.wireplumber.overrideAttrs (old: rec {
-        version = "0.5.12";
-        src = prev.fetchurl {
-          url = "https://gitlab.freedesktop.org/pipewire/wireplumber/-/archive/${version}/wireplumber-${version}.tar.gz";
-          hash = "sha256-DOXNSAh7xbVZ1+GpR+ngrbKptvHavZhK+AHzD7ul4Zw=";
-        };
-      });
+      # wireplumber = prev.wireplumber.overrideAttrs (old: rec {
+      #   version = "0.5.12";
+      #   src = prev.fetchurl {
+      #     url = "https://gitlab.freedesktop.org/pipewire/wireplumber/-/archive/${version}/wireplumber-${version}.tar.gz";
+      #     hash = "sha256-DOXNSAh7xbVZ1+GpR+ngrbKptvHavZhK+AHzD7ul4Zw=";
+      #   };
+      # });
 
       # Update codex: 0.92.0 -> 0.98.0
       # Uses importCargoLock instead of fetchCargoVendor because a git
@@ -40,24 +40,21 @@
         };
       });
 
-      # Fix trezor Python package for Python 3.13
-      # https://github.com/NixOS/nixpkgs/pull/455630
-      # pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-      #   (python-final: python-prev: {
-      #     trezor = python-prev.trezor.overrideAttrs (old: rec {
-      #       version = "0.20.0.dev0";
-      #       src = prev.fetchPypi {
-      #         pname = "trezor";
-      #         inherit version;
-      #         hash = "sha256-hU2J5TORWU55zoxjfsFPjk4VtNoxmVsjceDVvTKXKxI=";
-      #       };
-      #       build-system = [ python-prev.hatchling ];
-      #       propagatedBuildInputs = (prev.lib.remove prev.trezor-udev-rules (old.propagatedBuildInputs or [])) ++ [
-      #         python-prev.noiseprotocol
-      #       ];
-      #     });
-      #   })
-      # ];
+      # Work around Python package regressions after nixpkgs update.
+      pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+        (python-final: python-prev: {
+          telethon = python-prev.telethon.overridePythonAttrs (_: {
+            patches = [ ];
+          });
+
+          construct-classes = python-prev.construct-classes.overridePythonAttrs (old: {
+            postPatch = (old.postPatch or "") + ''
+              substituteInPlace pyproject.toml \
+                --replace-fail "uv_build>=0.8.13,<0.10.0" "uv_build>=0.8.13,<0.11.0"
+            '';
+          });
+        })
+      ];
     })
   ];
 }
