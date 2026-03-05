e3e29b4de952d4cf376f88339593134a91c94712
Pavel Shirshov <pshirshov@eml.cc>
2026-03-05 15:10:22 +0000

    autosave (pavel-am5 / Linux)

--- modules/nixos/virt-manager.nix
+++ modules/nixos/virt-manager.nix
@@ -117,6 +117,13 @@ in
           allowedBridges = lib.optional (mainBridge != null) mainBridge;
         };
       };
+
+      # libvirt ships this unit with /usr/bin/sh and unqualified binaries.
+      # On NixOS we force a store-pure command line so initialization can execute.
+      systemd.services.virt-secret-init-encryption.serviceConfig.ExecStart = lib.mkForce [
+        ""
+        "${pkgs.runtimeShell} -c 'umask 0077 && (${pkgs.coreutils}/bin/dd if=/dev/random status=none bs=32 count=1 | ${pkgs.systemd}/bin/systemd-creds encrypt --name=secrets-encryption-key - /var/lib/libvirt/secrets/secrets-encryption-key)'"
+      ];
     }
 
     (lib.mkIf cfg.iommu.enable {
