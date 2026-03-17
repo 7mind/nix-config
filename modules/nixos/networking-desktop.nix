e9b5ffd8c06b03ea596ee28f873a6791dc56ddf0
Pavel Shirshov <pshirshov@eml.cc>
2026-03-17 13:47:39 +0000

    autosave (pavel-fw / Linux)

--- modules/nixos/networking-desktop.nix
+++ modules/nixos/networking-desktop.nix
@@ -15,7 +15,9 @@
         networkmanager = {
           enable = true;
           wifi.backend = "wpa_supplicant";
-          unmanaged = [
+
+          # On desktops, only manage wifi; on laptops, manage everything
+          unmanaged = lib.mkIf (!config.smind.isLaptop) [
             "type:ethernet"
             "type:tun"
             "type:vlan"
@@ -26,15 +28,24 @@
             "except:interface-name:wlan*"
             "except:interface-name:enp*"
           ];
+
+          settings.connectivity = {
+            uri = "http://nmcheck.gnome.org/check_network_status.txt";
+            response = "NetworkManager is online";
+            interval = 300;
+          };
         };
 
         wireless.iwd.enable = false;
         wireless.enable = true;
       };
 
-      # NetworkManager does not manage ethernet, so GIO's default networkmanager
-      # monitor reports no connectivity. Use the base monitor instead.
-      environment.sessionVariables.GIO_USE_NETWORK_MONITOR = "base";
+      # NetworkManager does not manage ethernet on desktops, so GIO's default
+      # networkmanager monitor reports no connectivity. Use the base monitor instead.
+      # On laptops NM manages all interfaces, so the default monitor works correctly
+      # and is needed for captive portal detection.
+      environment.sessionVariables.GIO_USE_NETWORK_MONITOR =
+        if config.smind.isLaptop then "networkmanager" else "base";
 
       systemd.services.NetworkManager-wait-online.enable = false;
     })
