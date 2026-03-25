81b0041311314ef7de56fcbd1ae098bb6e24f485
Pavel Shirshov <pshirshov@eml.cc>
2026-03-25 11:48:19 +0000

    autosave (pavel-am5 / Linux)

--- modules/hm/tmux.nix
+++ modules/hm/tmux.nix
@@ -25,27 +25,25 @@
         # Catppuccin options — must be set BEFORE plugin loads (v2.1.3 API).
         # Use "custom" style so catppuccin doesn't override our separators.
         set -g @catppuccin_window_status_style "custom"
-        set -g @catppuccin_window_number_position "right"
+        set -g @catppuccin_window_number_position "left"
 
-        set -g @catppuccin_window_left_separator ""
-        set -g @catppuccin_window_right_separator " "
-        set -g @catppuccin_window_middle_separator " █"
+        set -g @catppuccin_window_left_separator " "
+        set -g @catppuccin_window_right_separator ""
+        set -g @catppuccin_window_middle_separator " "
 
-        # Compact: show marker when narrow, name when wide
-        set -g @catppuccin_window_text "#{?#{e|<|:#{client_width},80},=,#W}"
-        set -g @catppuccin_window_current_text "#{?#{e|<|:#{client_width},80},*,#W}"
+        # Tab text: directory_name:command_name
+        set -g @catppuccin_window_text "#{b:pane_current_path}:#{pane_current_command}"
+        set -g @catppuccin_window_current_text "#{b:pane_current_path}:#{pane_current_command}"
 
         set -g @catppuccin_status_left_separator " "
         set -g @catppuccin_status_right_separator ""
         set -g @catppuccin_status_connect_separator "no"
 
-        set -g @catppuccin_directory_text " #{pane_current_path}"
-
         # Load catppuccin after options are set
         run-shell ${pkgs.tmuxPlugins.catppuccin}/share/tmux-plugins/catppuccin/catppuccin.tmux
 
-        # Compact: hide status modules when narrow
-        set -g status-right "#{?#{e|<|:#{client_width},80},,#{E:@catppuccin_status_directory}#{E:@catppuccin_status_user}#{E:@catppuccin_status_host}#{E:@catppuccin_status_session}}"
+        # Wide: user@host:<dirname>, Narrow: nothing
+        set -g status-right "#{?#{e|<|:#{client_width},80},,#[fg=#{@thm_fg},bg=#{@thm_surface_0}] #(whoami)@#h:#{b:pane_current_path} }"
       '';
     };
   };
